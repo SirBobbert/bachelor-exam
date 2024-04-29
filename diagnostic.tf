2a5c89a055810ed867e1d31a65d3008468e44d23
SirBobbert <robert.pallesen13@gmail.com>
2024-04-29 09:59:03 +0200

    changes to main.tf

--- diagnostic.tf
+++ diagnostic.tf
@@ -1,4 +1,4 @@
-
+/*
 resource "azurerm_monitor_diagnostic_setting" "example" {
   name               = "example-psqlflexibleserver-diagnostics"
   target_resource_id = azurerm_postgresql_flexible_server.example.id
@@ -20,3 +20,4 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
     }
   }
 }
+*/
